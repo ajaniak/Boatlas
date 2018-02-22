5bb1c878a3b0a632534870e6c32e784840e79b43
Ella DUREAULT <elladureault@MacBook-Pro-de-Ella.local>
2018-02-22 17:21:36 +0100

    Ajout de l'import generic

--- gazetteer/app.py
+++ gazetteer/app.py
@@ -2,38 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
-from .constantes import CONFIG
+from .constantes import SECRET_KEY
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
 statics = os.path.join(chemin_actuel, "static")
 
-# On initie l'extension
-db = SQLAlchemy()
-# On met en place la gestion d'utilisateur-rice-s
-login = LoginManager()
-
 app = Flask(
-    __name__,
+    "Application",
     template_folder=templates,
     static_folder=statics
 )
+# On configure le secret
+app.config['SECRET_KEY'] = SECRET_KEY
+# On configure la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gazetteer_user:password@localhost/gazetteer'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# On initie l'extension
+db = SQLAlchemy(app)
+
+# On met en place la gestion d'utilisateur-rice-s
+login = LoginManager(app)
 
 
 from .routes import generic
 from .routes import api
-
-
-def config_app(config_name="test"):
-    """ Create the application """
-    app.config.from_object(CONFIG[config_name])
-
-    # Set up extensions
-    db.init_app(app)
-    # assets_env = Environment(app)
-    login.init_app(app)
-
-    # Register Jinja template functions
-
-    return app
-
