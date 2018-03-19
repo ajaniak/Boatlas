0b3c32cec32a4877f8277a4eb460b08c153c64e0
Ella DUREAULT <elladureault@MacBook-Pro-de-Ella.local>
2018-03-19 16:10:11 +0100

    Suppression sqldblite

--- gazetteer/routes.py
+++ gazetteer/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, flash, redirect
 
 
 from .app import app, login
-from .modeles.donnees import Place
+from .modeles.donnees import Place, Biblio
 from .modeles.utilisateurs import User
 from .constantes import LIEUX_PAR_PAGE
 from flask_login import login_user, current_user, logout_user, login_required
