fe1b0af8b108778939de6c538468697f47a4ff4f
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-03-28 18:43:39 +0200

    tentative de résolutions du pb de constraint...echec

--- gazetteer/donnees_sql/datamodel-following.sql
+++ gazetteer/donnees_sql/datamodel-following.sql
@@ -12,8 +12,8 @@ DROP TABLE IF EXISTS `gazetteer`.`link` ;
 CREATE TABLE IF NOT EXISTS `gazetteer`.`link` (
   `link_id` SMALLINT NOT NULL AUTO_INCREMENT,
   `link_place1_id` SMALLINT NOT NULL,
-  `link_relation_type_id` SMALLINT NOT NULL,
   `link_place2_id` SMALLINT NOT NULL,
+  `link_relation_type_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`link_id`),
   CONSTRAINT `fk_link_1`
@@ -21,14 +21,14 @@ CREATE TABLE IF NOT EXISTS `gazetteer`.`link` (
     REFERENCES `gazetteer`.`place` (`place_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    CONSTRAINT `fk_link_2`
-        FOREIGN KEY (`link_relation_type_id`)
-        REFERENCES `gazetteer`.`link_type` (`link_type_id`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-  CONSTRAINT `fk_link_3`
+  CONSTRAINT `fk_link_2`
     FOREIGN KEY (`link_place2_id`)
     REFERENCES `gazetteer`.`place` (`place_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_link_3`
+    FOREIGN KEY (`link_relation_type_id`)
+    REFERENCES `gazetteer`.`link_type` (`link_type_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
