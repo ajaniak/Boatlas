f0c8ede624e254114b9ace008644372b8720423d
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-05-01 18:34:56 +0200

    essaie de mettre en place la fonctionnalité de recherche par radius dans l'api... version problablement non fonctionnelle puisque version très simple par query sur la DB contrairement au modèle que l'on peut trouver sur internet avec des calculs très complexe en tuiles et autres...

--- gazetteer/routes/api.py
+++ gazetteer/routes/api.py
@@ -6,6 +6,7 @@ from ..constantes import LIEUX_PAR_PAGE, API_ROUTE
 from ..modeles.donnees import Place, Biblio, Relation, Authorship
 from ..modeles.utilisateurs import User
 
+
 #Classe pour encoder les sets en listes (pour la TypeError "JSON is not serializable")
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -92,3 +93,99 @@ def api_places_browse():
 
     response = jsonify(dict_resultats)
     return response
+
+@app.route(API_ROUTE+"/places/searchproximity")
+def api_places_browse_proximity():
+        """
+        On s'inspire du code pour la recherche en plein texte.
+        loc lieu à partir duquel l'utilisateur souhaite faire sa recherche: il a une latitude et une longitude
+        radius: le rayon de recherche de l'utilisateur
+        """
+    #il faut ajouter la capacité pour l'utilisateur de donner le radius et de préciser la latitude et la longitude qu'il veut voir appliquerself.
+    #seulement je ne vois pas comment faire cette demande dans le cadre de l'API.
+    loc_latitude = request.args.get("y", None)
+    loc_longitude = request.args.get("x", None)
+    radius = request.args.get("q", None)
+    page = request.args.get("page", 1)
+#on conserve la partie sur les gestions des pages afin de gérer l'affiche des résultats (enfin je crois...)
+    if isinstance(page, str) and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+# si les infos nécessaires pour faire le query sur la DB sont manquantes, le code renvoie toutes les places.
+    if not loc_latitude:
+        query = Place.query
+
+    if not loc_longitude:
+        query = Place.query
+
+    if not radius:
+        query = Place.query
+
+    if loc_latitude and loc_longitude and radius:
+            pointofsearch = Place.query.filter(func.acos(func.sin(func.radians(loc_latitude)) * func.sin(func.radians(Place.place_latitude)) + func.cos(func.radians(loc_latitude)) * func.cos(func.radians(Place.place_latitude)) * func.cos(func.radians(Place.place_longitude) - (func.radians(loc_longitude)))) * 6371 <= radius)
+
+#ce bout de code est conservé de la fonction si dessus par défaut.
+    try:
+        resultats = pointofsearch.paginate(page=page, per_page=LIEUX_PAR_PAGE)
+    except Exception:
+        return Json_404()
+
+#difficulté de visualisation de l'organisation des données récupérées par le pointofsearch. Tentative d'adaptation de la réponse du code pour la recherche plein texte.
+    dict_resultats = {
+        "links": {
+            "self": request.url
+        },
+        "data": [
+            place.to_jsonapi_dict()
+            for place in resultats.items
+        ]
+    }
+#je ne suis pas certaine que l'on puisse mettre plusieurs varibables dans le if de gestions des pages de résultats....
+    if resultats.has_next:
+        arguments = {
+            "page": resultats.next_num
+        }
+        if loc_latitude and loc_longitude and radius:
+            arguments["x"] = loc_latitude
+            arguments["y"] = loc_longitude
+            arguments["q"] = radius
+        dict_resultats["links"]["next"] = url_for("api_places_browse_proximity", _external=True)+"?"+urlencode(arguments)
+
+    if resultats.has_prev:
+        arguments = {
+            "page": resultats.prev_num
+        }
+        if loc_latitude and loc_longitude and radius:
+            arguments["x"] = loc_latitude
+            arguments["y"] = loc_longitude
+            arguments["q"] = radius
+        dict_resultats["links"]["prev"] = url_for("api_places_browse_proximity", _external=True)+"?"+urlencode(arguments)
+
+    response = jsonify(dict_resultats)
+    return response
+
+@app.route(API_ROUTE+"/places/searchbbox")
+def api_places_browse_bbox():
+    # on tente d'ajouter une fonctionalité de recherche par zone avec 4 coordonnées soit 2 latitudes et 2 longitudes chacun représentant un minimum et un maximum...
+
+  results = []
+
+  if cost_function is None:
+    cost_function = default_cost_function
+  query_geocells = geocell.best_bbox_search_cells(bbox, cost_function)
+
+  if query_geocells:
+    for entity in query.filter('location_geocells IN', query_geocells):
+      if len(results) == max_results:
+        break
+      if (entity.location.lat >= bbox.south and
+          entity.location.lat <= bbox.north and
+          entity.location.lon >= bbox.west and
+          entity.location.lon <= bbox.east):
+        results.append(entity)
+
+  if DEBUG:
+    logging.info('bbox query looked in %d geocells' % len(query_geocells))
+
+  return results
