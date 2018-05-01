90c271d8b4c6186fd106d20f340d3c46a8431ad2
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-05-01 16:21:58 +0200

    mise à jr du modele pour faire les liaisons- non effectif

--- gazetteer/donnees_sql/datamodel-following.sql
+++ gazetteer/donnees_sql/datamodel-following.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS `gazetteer`.`link` ;
 CREATE TABLE IF NOT EXISTS `gazetteer`.`link` (
   `link_id` INT NOT NULL AUTO_INCREMENT,
   `link_relation_type` VARCHAR(45) NOT NULL,
-  `link_relation_description`VARCHAR(240),
-  PRIMARY KEY (`link_id`),
+  `link_relation_description` VARCHAR(240),
+  PRIMARY KEY (`link_id`))
 ENGINE = InnoDB;
