1a452a89f3f1324da01cc3496327e6a4ed7139c5
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-06-18 21:28:34 +0200

    transfert des données sql vers un unique fichier datamodel

--- gazetteer/donnees_sql/datamodel-following.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `gazetteer`.`link` ;
-
-CREATE TABLE IF NOT EXISTS `gazetteer`.`link` (
-  `link_id` INT NOT NULL AUTO_INCREMENT,
-  `link_relation_type` VARCHAR(45) NOT NULL,
-  `link_relation_description` VARCHAR(240),
-  PRIMARY KEY (`link_id`))
-ENGINE = InnoDB;
