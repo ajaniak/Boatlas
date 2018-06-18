51e0c2af0f82506225089efbe62cd307c0e7eb6f
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-06-18 22:11:58 +0200

    mise en commentaire de la partie message d'erreur vers une boîte mail

--- gazetteer/__init__.py
+++ gazetteer/__init__.py
@@ -1,3 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
@@ -19,3 +24,4 @@ if not app.debug:
     mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'gazetteer failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+    """
