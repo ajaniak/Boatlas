59f82214087b24d8a20d78f558f1b2a124bca1a3
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-06-18 22:13:00 +0200

    mise en commentaire du code non fonctionnel pour l'efficacité du query

--- gazetteer/app.py
+++ gazetteer/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
-from .constantes import SECRET_KEY, config
+from .constantes import SECRET_KEY, CONFIG
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
@@ -33,12 +33,15 @@ def config_app(config_name="test"):
     """ Create the application """
     app.config.from_object(CONFIG[config_name])
 
-    # On initie les extensions
+    # Set up extensions
     db.init_app(app)
+    # assets_env = Environment(app)
     login.init_app(app)
 
-    return app
+    # Register Jinja template functions
 
+    return app
+"""
 # configuration pour la performance du sql via la fonction get_debug_queries de SQLALCHEMY_DATABASE_URI
 SQLALCHEMY_RECORD_QUERIES = True
 #configuration de la limite au dela de laquelle la durée de query est trop longue, en seconde:
@@ -51,3 +54,4 @@ MAIL_USERNAME = 'ajaniak' , 'e.dureault'
 MAIL_PASSWORD = None #se renseigner sur la manière de gérer le mot de passe.
 #mailing liste
 ADMINS = ['axelle.janiak@chartes.psl.eu', 'ella.duréault@chartes.psl.eu']
+"""
