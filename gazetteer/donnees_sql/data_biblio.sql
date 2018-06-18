aad036e781f96d024cfcf607bfcab509c318eefb
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-06-18 21:27:19 +0200

    transfert des données de ce fichier dans un unique fichier datamodel

--- gazetteer/donnees_sql/data_biblio.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`,`biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 1, 'Versailles, un palais pour la sculpture', 'Alexandre Maral', '2013', 'Dijon', 'art history');
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`,`biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 2, 'The Banquet', 'Platon', 'Ve BC.', 'Athens', 'philosophical treaty');
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`,`biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 3, 'Le Phèdre','Platon', 'Ve BC.', 'Athens', 'philosophical treaty');
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`, `biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 4, 'The Iliad', 'Homer', '', '', 'epic');
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`, `biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 5, 'The Odyssey', 'Homer', '', '', 'epic');
-INSERT INTO `gazetteer`.`biblio` (`biblio_id`, `biblio_titre`, `biblio_auteur`, `biblio_date`, `biblio_lieu`, `biblio_type`) VALUES ( 6, 'Works and Days', 'Hesiod', '', '', 'poem');
