d5149380c1db46f5728dd8781d3e8f2668ac33f1
Ella DUREAULT <ella.dureault@gmail.com>
2018-06-21 19:50:55 +0200

    Ajout d'une route (après la fonction dans les données) pour afficher l'index des biblios

--- gazetteer/routes/api.py
+++ gazetteer/routes/api.py
@@ -82,7 +82,7 @@ def api_places_browse():
             "self": request.url
         },
         "data": [
-            place.to_jsonapi_dict()
+            place.to_jsonapi_dict_2()
             for place in resultats.items
         ]
     }
@@ -102,6 +102,63 @@ def api_places_browse():
         if motclef:
             arguments["q"] = motclef
         dict_resultats["links"]["prev"] = url_for("api_places_browse", _external=True)+"?"+urlencode(arguments)
+
+    response = jsonify(dict_resultats)
+    return response
+
+@app.route(API_ROUTE+"/biblios")
+def api_biblios_browse():
+    """ Route permettant la recherche plein-texte
+
+    On s'inspirera de http://jsonapi.org/ faute de pouvoir trouver le
+    temps d'y coller à 100%
+    """
+    # q est très souvent utilisé pour indiquer une capacité de recherche
+    motclef = request.args.get("q", None)
+    page = request.args.get("page", 1)
+
+    if isinstance(page, str) and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    if motclef:
+        query = Biblio.query.filter(
+            Biblio.biblio_titre.like("%{}%".format(motclef))
+        )
+    else:
+        query = Biblio.query
+
+    try:
+        resultats = query.paginate(page=page, per_page=LIEUX_PAR_PAGE)
+    except Exception:
+        return Json_404()
+
+    dict_resultats = {
+        "links": {
+            "self": request.url
+        },
+        "data": [
+            biblio.to_jsonapi_dict_2()
+            for biblio in resultats.items
+        ]
+    }
+
+    if resultats.has_next:
+        arguments = {
+            "page": resultats.next_num
+        }
+        if motclef:
+            arguments["q"] = motclef
+        dict_resultats["links"]["next"] = url_for("api_biblios_browse", _external=True)+"?"+urlencode(arguments)
+
+    if resultats.has_prev:
+        arguments = {
+            "page": resultats.prev_num
+        }
+        if motclef:
+            arguments["q"] = motclef
+        dict_resultats["links"]["prev"] = url_for("api_biblios_browse", _external=True)+"?"+urlencode(arguments)
 
     response = jsonify(dict_resultats)
     return response
