ea074883a8478d8ead84dd22e94a81eff2c38f15
Ella DUREAULT <ella.dureault@gmail.com>
2018-06-21 19:50:11 +0200

    Ajout d'un index pour les biblios sur le modèle proposé pour les lieux

--- gazetteer/modeles/donnees.py
+++ gazetteer/modeles/donnees.py
@@ -52,6 +52,25 @@ class Place(db.Model):
              }
         return data
 
+    def to_jsonapi_dict_2(self):
+        data = {
+            "type": "place",
+            "id": self.place_id,
+            "attributes": {
+                "name": self.place_nom,
+                "description": self.place_description,
+                "longitude": self.place_longitude,
+                "latitude": self.place_latitude,
+                "category": self.place_type,
+
+            },
+            "links": {
+                "api": url_for("get_place", id=self.place_id, _external=True),
+                "self": url_for("lieu", place_id=self.place_id, _external=True)
+            }
+        }
+        return data
+
     def dictionnaire_2(self):
         """ Permet d'éviter une boucle infinie lors
         de l'établissement des relations
@@ -75,8 +94,6 @@ class Place(db.Model):
                     }
         }
         return data
-
-
 
 
     @staticmethod
@@ -189,6 +206,24 @@ class Biblio(db.Model):
                               for endroit in self.relations
                           ]
                      }
+        return data
+
+    def to_jsonapi_dict_2(self):
+        data = {
+            "type": "biblio",
+            "id": self.biblio_id,
+            "attributes": {
+                "titre": self.biblio_titre,
+                "auteur": self.biblio_auteur,
+                "date": self.biblio_date,
+                "lieu": self.biblio_lieu,
+                "category": self.biblio_type,
+
+            },
+            "links": {
+                "self": url_for("biblio", biblio_id=self.biblio_id, _external=True),
+                "json": url_for("get_biblio", id=self.biblio_id, _external=True)
+            }}
         return data
 
     def dictionnaire_2(self):
