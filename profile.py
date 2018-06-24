224c358752de9ed31b8fe1ce3ebc2e96462f485e
ajaniak <axelle.janiak@enc-sorbonne.fr>
2018-06-24 22:08:29 +0200

    le profile ne fonctionne pas, je n'arrive pas à installer profile ou cProfile, mais pour le coup pas de problèmes avec pstat

--- profile.py
+++ profile.py
@@ -1,3 +1,4 @@
+import cProfile
 from werkzeug.contrib.profiler import ProfilerMiddleware
 from gazetteer.app import app
 
@@ -5,3 +6,5 @@ app.config['PROFILE'] = True
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[25])
 app.run(debug = True)
 #restriction sur 25 fonctions lourdes, même si je ne suis pas certaine du nombre dans notre appself.
+
+#non fonctionnel, j'arrive à installer pstat mais par cProfile ou profileself.
